bdb00da5c1d947908964deabccc5aafae00b89bc
Camille Doncieux <33500924+DONCIEUX-CAMILLE@users.noreply.github.com>
2022-07-27 23:23:59 +0200

    Create zabbix.conf.php

---
+++ spk/zabbix/src/zabbix.conf.php
@@ -0,0 +1,16 @@
+<?php
+// Zabbix GUI configuration file
+global $DB;
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = '@db_zabbix_server_name@';
+$DB['USER']     = '@db_zabbix_server_user@';
+$DB['PASSWORD'] = '@db_password@';
+// SCHEMA is relevant only for IBM_DB2 database
+$DB['SCHEMA'] = '';
+$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'Zabbix server';
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+?>
